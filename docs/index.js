a12296eb0a746bc72bcb2bafcfb17b6b4e77cad2
konjacdesert5289 <56007373+konjacdesert5289@users.noreply.github.com>
2021-02-24 23:32:31 +0900

    キャンバスで拡大したバージョン

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,36 @@
 window.addEventListener("load", function () {
-    var q_split = location.search.substring(1).split('&');
-    for (var _i = 0, q_split_1 = q_split; _i < q_split_1.length; _i++) {
-        var q = q_split_1[_i];
-        var qm = q.split("=");
-        if (qm[0] == "m") {
-            var mes = document.getElementById("mes");
-            if (mes) {
-                mes.innerText = decodeURI(qm[1]).replace("\n", "");
-                mes.style.animationDuration = Math.max((mes.clientWidth - document.body.clientWidth) / mes.clientHeight * 2.5, 10) + "s";
-            }
+    var mes = (function () {
+        var q_split = location.search.substring(1).split("&");
+        for (var _i = 0, q_split_1 = q_split; _i < q_split_1.length; _i++) {
+            var q = q_split_1[_i];
+            var qm = q.split("=");
+            if (qm[0] == "m")
+                return decodeURI(qm[1]).replace("\n", "");
         }
+        return "";
+    })();
+    console.log(mes);
+    var size = (function () {
+        var sizedom = document.createElement("span");
+        sizedom.innerText = mes;
+        document.body.appendChild(sizedom);
+        var r = { w: sizedom.offsetWidth, h: sizedom.offsetHeight };
+        // document.body.removeChild(sizedom);
+        return r;
+    })();
+    console.log(size);
+    var textcanvas = document.createElement("canvas");
+    textcanvas.width = size.w;
+    textcanvas.height = size.h;
+    var ctx = textcanvas.getContext("2d", { alpha: false });
+    if (ctx) {
+        ctx.font = "24px serif";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#F80";
+        ctx.fillText(mes, 0, size.h / 2);
     }
+    document.body.appendChild(textcanvas);
+    var filter = document.getElementById("f");
+    if (filter)
+        filter.style.backgroundSize = 100 / size.h + "vh " + 100 / size.h + "vh";
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,37 @@
 window.addEventListener("load", () => {
-  const q_split = location.search.substring(1).split('&');
-  for (const q of q_split) {
-    const qm = q.split("=");
-    if (qm[0] == "m") {
-      const mes = document.getElementById("mes");
-      if (mes) {
-        mes.innerText = decodeURI(qm[1]).replace("\n", "");
-        mes.style.animationDuration = `${Math.max((mes.clientWidth - document.body.clientWidth) / mes.clientHeight * 2.5, 10)}s`
-      }
+  const mes = (() => {
+    const q_split = location.search.substring(1).split("&");
+    for (const q of q_split) {
+      const qm = q.split("=");
+      if (qm[0] == "m") return decodeURI(qm[1]).replace("\n", "");
     }
+    return "";
+  })();
+  console.log(mes);
+
+  const size = (() => {
+    const sizedom = document.createElement("span");
+    sizedom.innerText = mes;
+    document.body.appendChild(sizedom);
+    const r = { w: sizedom.offsetWidth, h: sizedom.offsetHeight };
+    // document.body.removeChild(sizedom);
+    return r;
+  })();
+  console.log(size);
+
+  const textcanvas = document.createElement("canvas");
+  textcanvas.width = size.w;
+  textcanvas.height = size.h;
+  const ctx = textcanvas.getContext("2d", { alpha: false });
+  if (ctx) {
+    ctx.font = "24px serif";
+    ctx.textBaseline = "middle";
+    ctx.fillStyle = "#F80";
+    ctx.fillText(mes, 0, size.h / 2);
   }
-})
+  document.body.appendChild(textcanvas);
+
+  const filter = document.getElementById("f");
+  if (filter)
+    filter.style.backgroundSize = `${100 / size.h}vh ${100 / size.h}vh`;
+});
